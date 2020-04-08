fa990036e48abac58cf1d6bf54f24f225e7f0c68
daonez <cyongchoi@gmail.com>
2020-04-08 19:39:32 +0900

    finished tutorial

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const reducer = (state = [], action) => {
     case ADD_TODO:
       return [...state,{text:action.text, id:Date.now()}];
     case DELETE_TODO:
-      return []     
+      return state.filter(toDo=> toDo.id !== action.id)     
     default:
       return state;
   }
@@ -45,7 +45,7 @@ const dispatchAddToDo = (text) => {
 };
 
 const dispatchDeleteToDo=(e)=>{
-  const id = e.target.parentNode.id;
+  const id = parseInt(e.target.parentNode.id);
   store.dispatch(deleteToDo(id))
 }
 
